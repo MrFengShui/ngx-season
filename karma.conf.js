1ae0972e18aa153f2ee649bc393253abf3f22f4b
MrFengShui <luansongjian@gmail.com>
2022-06-19 11:08:39 +0800

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/octpus'),
+      dir: require('path').join(__dirname, './coverage/octopus'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'octpus'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('octpus');
+    expect(app.title).toEqual('octopus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('octpus app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('octopus app is running!');
   });
 });
